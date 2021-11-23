cb85b37943e52a88abced3f8e2f57a250eb04c25
Xiantao Ge <gexiantao@agora.io>
2021-11-23 11:47:11 +0800

    change install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION=0.6.0
+VERSION=0.0.1
 
 docker login
 
 # docker build -f ObserverBuilder.dockerfile . -t observertc/webrtc-observer:$VERSION
-gradle dockerBuild
-docker push observertc/webrtc-observer:$VERSION
+./gradlew dockerBuild
+docker push gavin0127/observer:$VERSION
